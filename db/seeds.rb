0501715de496eaeb31dd9a6daabc26952e5c285f
BiLaNuK <bilanuk.bill@gmail.com>
2022-06-14 02:29:28 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+obj = Track.create(
+  title: 'Cradles',
+  author: 'Sub Urban',
+)
+obj.track.attach(io: File.open('app/assets/tracks/Cradles.mp3'), filename: 'Cradles.mp3', content_type: 'audio/mp3')
+obj.image_cover.attach(io: File.open('app/assets/images/cradles.jpeg'), filename: 'cradles.jpeg', content_type: 'image/jpeg')
+
+obj = Track.create(
+  title: 'Uptown funk',
+  author: 'Bruno Mars',
+  )
+obj.track.attach(io: File.open('app/assets/tracks/test_audio.mp3'), filename: 'test_audio.mp3', content_type: 'audio/mp3')
+obj.image_cover.attach(io: File.open('app/assets/images/bruno-mars.png'), filename: 'bruno-mars.png', content_type: 'image/png')
+
+obj = Track.create(
+  title: 'Keep Going',
+  author: 'Sworn',
+  )
+obj.track.attach(io: File.open('app/assets/tracks/sworn-chill.mp3'), filename: 'sworn-chill.mp3', content_type: 'audio/mp3')
+obj.image_cover.attach(io: File.open('app/assets/images/sworn2.jpeg'), filename: 'sworn2.jpeg', content_type: 'image/jpeg')
